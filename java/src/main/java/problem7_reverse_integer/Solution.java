d91d86b9c6f50f432c1f014ed9e1bbce98cf5e02
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-11-27 14:17:08 +0900

    solve problem7

---
+++ java/src/main/java/problem7_reverse_integer/Solution.java
@@ -0,0 +1,20 @@
+package problem7_reverse_integer;
+
+public class Solution {
+  public int reverse(int x) {
+    try {
+      if (x == Integer.MIN_VALUE) {
+        return 0;
+      }
+      StringBuffer sb = new StringBuffer(String.valueOf(Math.abs(x)));
+      int ans = Integer.parseInt(sb.reverse().toString());
+      if (x < 0) {
+        return -ans;
+      } else {
+        return ans;
+      }
+    } catch (NumberFormatException e) {
+      return 0;
+    }
+  }
+}
